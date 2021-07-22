28f85c68c01229b6b780f8ea668dc63967740db3
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-07-22 13:34:46 +0900

    api通信に対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:labo_flutter/project_detail_page.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(
@@ -97,6 +101,42 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+Future<Album> fetchAlbum() async {
+  final response =
+  await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+    );
+  }
+
+  final int userId;
+  final int id;
+  final String title;
+}
+
+
 class TabPage extends StatefulWidget {
   const TabPage({Key? key, required this.title, required this.color}) :
         super(key: key);
@@ -109,6 +149,15 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+    log(futureAlbum.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -137,7 +186,22 @@ class _TabPageState extends State<TabPage> {
                 );
               },
               child: const Text('次へ'),
-            )
+            ),
+            FutureBuilder<Album>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final title = snapshot.data!.title;
+                  final userId = snapshot.data!.userId.toString();
+                  return Text('Title: $title,\nUserId: $userId');
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),
           ],
         ),
       ),
