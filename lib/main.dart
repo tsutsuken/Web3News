98bc795bdc13495730e628ba45dd09d77ec0cac0
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-07-22 16:45:43 +0900

    FutureProviderに対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:labo_flutter/project_detail_page.dart';
 import 'package:http/http.dart' as http;
@@ -101,20 +101,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Future<Album> fetchAlbum() async {
+final albumProvider = FutureProvider<Album>((ref) async {
   final response =
   await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
-}
+});
 
 class Album {
   Album({
@@ -137,7 +133,7 @@ class Album {
 }
 
 
-class TabPage extends StatefulWidget {
+class TabPage extends HookWidget {
   const TabPage({Key? key, required this.title, required this.color}) :
         super(key: key);
 
@@ -145,29 +141,17 @@ class TabPage extends StatefulWidget {
   final Color color;
 
   @override
-  _TabPageState createState() => _TabPageState();
-}
-
-class _TabPageState extends State<TabPage> {
-  late Future<Album> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-    log(futureAlbum.toString());
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final albumAsyncValue = useProvider(albumProvider);
+
     return Scaffold(
-      backgroundColor: widget.color,
+      backgroundColor: color,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-                widget.title,
+                title,
                 style: const TextStyle(
                     color: Colors.white,
                     fontSize: 36,
@@ -180,26 +164,24 @@ class _TabPageState extends State<TabPage> {
                   MaterialPageRoute<void>(
                       builder: (context) =>
                           ProjectDetailPage(
-                              title: widget.title,
-                              color: widget.color)
+                              title: title,
+                              color: color)
                   ),
                 );
               },
               child: const Text('次へ'),
             ),
-            FutureBuilder<Album>(
-              future: futureAlbum,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final title = snapshot.data!.title;
-                  final userId = snapshot.data!.userId.toString();
-                  return Text('Title: $title,\nUserId: $userId');
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
-
-                // By default, show a loading spinner.
-                return const CircularProgressIndicator();
+            albumAsyncValue.when(
+              data: (data) {
+                final title = data.title;
+                final userId = data.userId.toString();
+                return Text('Title: $title,\nUserId: $userId');
+              },
+              loading: () {
+                return const Text('Loading');
+              },
+              error: (e, stackTrace) {
+                return Text(e.toString());
               },
             ),
           ],
