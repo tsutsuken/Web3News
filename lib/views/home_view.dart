38272587b0a96cee661f4650483494a8eb56b9c5
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-07-23 20:47:52 +0900

    ListViewを追加

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,12 +5,38 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[Text('HomeView')]),
-      ),
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return _listItem('いよいよ寿命？ 40年以上の使用に耐えたエアコン「霧ヶ峰」が懐かしすぎると話題に',
+            const Icon(Icons.settings));
+      },
+    );
+  }
+
+  Widget _listItem(String title, Icon icon) {
+    return GestureDetector(
+      onTap: () {
+        print('onTap');
+      },
+      child: Container(
+          height: 120,
+          padding: const EdgeInsets.all(8),
+          decoration: const BoxDecoration(
+              border: Border(bottom: BorderSide(width: 1, color: Colors.grey))),
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(0),
+                child: Image.network('http://placehold.jp/200x200.png'),
+              ),
+              Flexible(
+                child: Text(
+                  title,
+                  style: const TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
