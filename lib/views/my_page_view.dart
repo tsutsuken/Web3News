e99c960c6408229a345cb89808410075acc4d0d9
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-07-28 19:19:09 +0900

    ユーザ情報をマイページに表示

--- lib/views/my_page_view.dart
+++ lib/views/my_page_view.dart
@@ -1,15 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class MyPageView extends StatelessWidget {
+class MyPageView extends HookWidget {
   const MyPageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final currentUser = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[Text('MyPageView')]),
+            children: <Widget>[
+              Text('currentUser: $currentUser'),
+              const Text('MyPageView')
+            ]),
       ),
     );
   }
