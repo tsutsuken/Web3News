962377d6a1896f9ef3894936a1af14bccd4198ec
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-07-29 20:46:54 +0900

    固定のメールアドレスでサインアップ

--- lib/views/my_page_view.dart
+++ lib/views/my_page_view.dart
@@ -9,13 +9,33 @@ class MyPageView extends HookWidget {
   Widget build(BuildContext context) {
     final currentUser = FirebaseAuth.instance.currentUser;
 
+    Future<UserCredential> createUserWithEmailAndPassword() async {
+      try {
+        final userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+                email: 'barry.allen@example.com',
+                password: 'SuperSecretPassword!');
+        return userCredential;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          print('The account already exists for that email.');
+        }
+        rethrow;
+      }
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text('currentUser: $currentUser'),
-              const Text('MyPageView')
+              const Text('MyPageView'),
+              ElevatedButton(
+                  onPressed: createUserWithEmailAndPassword,
+                  child: const Text('サインアップ'))
             ]),
       ),
     );
