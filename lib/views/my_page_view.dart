f0fa56d0c80547b04751ef3714ec1daedc1165b8
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-08-01 18:17:46 +0900

    マイページ画面に投稿一覧を表示

--- lib/views/my_page_view.dart
+++ lib/views/my_page_view.dart
@@ -1,6 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+const String postsQuery = '''
+{
+  posts {
+    id
+    text
+  }
+}
+''';
 
 class MyPageView extends HookWidget {
   const MyPageView({Key? key}) : super(key: key);
@@ -35,7 +45,39 @@ class MyPageView extends HookWidget {
               const Text('MyPageView'),
               ElevatedButton(
                   onPressed: createUserWithEmailAndPassword,
-                  child: const Text('サインアップ'))
+                  child: const Text('サインアップ')),
+              Query(
+                options: QueryOptions(
+                  document: gql(postsQuery),
+                  pollInterval: const Duration(seconds: 10),
+                ),
+                builder: (QueryResult result,
+                    {VoidCallback? refetch, FetchMore? fetchMore}) {
+                  if (result.hasException) {
+                    return Text(result.exception.toString());
+                  }
+
+                  if (result.isLoading) {
+                    return const Text('Loading');
+                  }
+
+                  final posts = result.data?['posts'] as List<dynamic>;
+                  return Flexible(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: posts.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final dynamic post = posts[index];
+                          return ListTile(
+                            title: Text('${post["text"]}'),
+                            trailing: const Icon(Icons.more_vert),
+                            subtitle: Text('${post["id"]}'),
+                            onTap: () {},
+                          );
+                        }),
+                  );
+                },
+              ),
             ]),
       ),
     );
--- lib/views/playground_view.dart
+++ lib/views/playground_view.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:labo_flutter/models/album/album.dart';
 import 'package:labo_flutter/models/album/album_repository.dart';
 import 'package:labo_flutter/views/playground_detail_view.dart';
-
-const String postsQuery = '''
-{
-  posts {
-    id
-    text
-  }
-}
-''';
 
 final albumProvider = FutureProvider<Album>((ref) async {
   final albumRepository = ref.read(albumRepositoryProvider);
@@ -61,38 +51,6 @@ class PlaygroundView extends HookWidget {
               },
               error: (e, stackTrace) {
                 return Text(e.toString());
-              },
-            ),
-            Query(
-              options: QueryOptions(
-                document: gql(postsQuery),
-                pollInterval: const Duration(seconds: 10),
-              ),
-              builder: (QueryResult result,
-                  {VoidCallback? refetch, FetchMore? fetchMore}) {
-                if (result.hasException) {
-                  return Text(result.exception.toString());
-                }
-
-                if (result.isLoading) {
-                  return const Text('Loading');
-                }
-
-                final posts = result.data?['posts'] as List<dynamic>;
-                return Flexible(
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: posts.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final dynamic post = posts[index];
-                        return ListTile(
-                          title: Text('${post["text"]}'),
-                          trailing: const Icon(Icons.more_vert),
-                          subtitle: Text('${post["id"]}'),
-                          onTap: () {},
-                        );
-                      }),
-                );
               },
             ),
           ],
