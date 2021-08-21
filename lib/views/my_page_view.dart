495850084217829142e44bd35badd8442bc687ce
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-08-21 19:22:52 +0900

    Userのfreezedに対応

--- lib/views/my_page_view.dart
+++ lib/views/my_page_view.dart
@@ -1,8 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide User;
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:labo_flutter/models/user/user.dart';
 import 'package:labo_flutter/providers/user_notifier_provider.dart';
 import 'package:labo_flutter/views/sign_in_view.dart';
 import 'package:labo_flutter/views/sign_up_view.dart';
@@ -69,13 +70,21 @@ class MyPageView extends HookWidget {
                     return const Text('Loading');
                   }
 
-                  final dynamic user = result.data?['users_by_pk'];
-                  final name = user['name'] as String ?? '';
-                  final id = user['id'] as String ?? '';
-                  print('user: ${user['id']}');
-                  return Text('name: $name, id: $id');
+                  final resultData =
+                      result.data?['users_by_pk'] as Map<String, dynamic>?;
+                  if (resultData == null) {
+                    return const Text('No User');
+                  }
+
+                  final user = User.fromJson(resultData);
+                  return Text('name: ${user.name}, id: ${user.id}');
                 },
               ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute<void>(
               if (currentUser == null) ...[
                 ElevatedButton(
                     onPressed: () {
