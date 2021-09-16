d73900bb21f76abc514984d9d7a3d1de1ab66930
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-09-16 12:11:02 +0900

    リファクタ: 記事詳細画面

--- lib/views/article_detail_view.dart
+++ lib/views/article_detail_view.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:labo_flutter/views/comment_list_view.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:labo_flutter/views/comment_create_view.dart';
+import 'package:labo_flutter/views/comment_list_view.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ArticleDetailView extends StatefulWidget {
+class ArticleDetailView extends HookWidget {
   const ArticleDetailView(
       {Key? key, required this.articleId, required this.articleUrl})
       : super(key: key);
@@ -14,65 +15,70 @@ class ArticleDetailView extends StatefulWidget {
   final String articleUrl;
 
   @override
-  _ArticleDetailViewState createState() => _ArticleDetailViewState();
-}
+  Widget build(BuildContext context) {
+    useEffect(() {
+      if (Platform.isAndroid) {
+        // Androidで日本語入力を可能にするため
+        WebView.platform = SurfaceAndroidWebView();
+      }
+    }, const []);
 
-class _ArticleDetailViewState extends State<ArticleDetailView> {
-  @override
-  void initState() {
-    super.initState();
-    // Enable hybrid composition.
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: buildWebView(),
+      bottomNavigationBar: _buildBottomAppBar(context),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ArticleDetailView'),
-      ),
-      body: WebView(
-        initialUrl: widget.articleUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-        gestureNavigationEnabled: true,
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.blue,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                color: Colors.white,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<void>(
-                      builder: (context) => CommentCreateView(
-                        articleId: widget.articleId,
-                      ),
-                      fullscreenDialog: true,
+  AppBar buildAppBar() {
+    return AppBar(
+      title: const Text('ArticleDetailView'),
+    );
+  }
+
+  WebView buildWebView() {
+    return WebView(
+      initialUrl: articleUrl,
+      javascriptMode: JavascriptMode.unrestricted,
+      gestureNavigationEnabled: true,
+    );
+  }
+
+  BottomAppBar _buildBottomAppBar(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.blue,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+              color: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (context) => CommentCreateView(
+                      articleId: articleId,
                     ),
-                  );
-                },
-                icon: const Icon(Icons.add_comment)),
-            IconButton(
-                color: Colors.white,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<void>(
-                      builder: (context) => CommentListView(
-                        articleId: widget.articleId,
-                      ),
-                      fullscreenDialog: true,
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              icon: const Icon(Icons.add_comment)),
+          IconButton(
+              color: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (context) => CommentListView(
+                      articleId: articleId,
                     ),
-                  );
-                },
-                icon: const Icon(Icons.chat))
-          ],
-        ),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              icon: const Icon(Icons.chat))
+        ],
       ),
     );
   }
