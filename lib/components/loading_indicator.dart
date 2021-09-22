6fc0d1dcbd856ea666276886c154ea1caeba9de2
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-09-22 17:28:25 +0900

    コメント一覧画面にpull_to_refreshを導入

---
+++ lib/components/loading_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        SizedBox(height: 96),
+        CircularProgressIndicator(),
+      ],
+    );
+  }
+}
--- lib/views/comment_list_view.dart
+++ lib/views/comment_list_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:labo_flutter/components/loading_indicator.dart';
 import 'package:labo_flutter/models/comment/comment.dart';
 import 'package:labo_flutter/utils/app_colors.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 const String commentsQuery = '''
   query MyQuery(\$article_id: String!) {
@@ -13,53 +16,62 @@ const String commentsQuery = '''
   }
 ''';
 
-class CommentListView extends StatelessWidget {
+class CommentListView extends HookWidget {
   const CommentListView({Key? key, required this.articleId}) : super(key: key);
 
   final String articleId;
 
   @override
   Widget build(BuildContext context) {
+    final _refreshControllerNotifier =
+        useState<RefreshController>(RefreshController(initialRefresh: false));
+
+    Future<void> _refreshList(VoidCallback? _refetch) async {
+      _refreshControllerNotifier.value.refreshCompleted();
+      if (_refetch != null) {
+        _refetch();
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('CommentListView')),
-      body: Column(
-        children: [
-          Query(
-              options: QueryOptions(
-                document: gql(commentsQuery),
-                variables: <String, dynamic>{
-                  'article_id': articleId,
-                },
-              ),
-              builder: (QueryResult result,
-                  {VoidCallback? refetch, FetchMore? fetchMore}) {
-                if (result.hasException) {
-                  return Text(result.exception.toString());
-                }
+      body: _buildBody(_refreshControllerNotifier.value, _refreshList),
+    );
+  }
 
-                if (result.isLoading) {
-                  return const Text('Loading');
-                }
-
-                var comments = <Comment>[];
-                final resultData = result.data;
-                if (resultData != null) {
-                  comments = CommentListResponse.fromJson(resultData).comments;
-                }
-
-                return RefreshIndicator(
-                  onRefresh: () async {
-                    if (refetch != null) {
-                      refetch();
-                    }
-                  },
-                  child: Flexible(
-                    child: _buildListView(comments),
-                  ),
-                );
-              })
-        ],
+  Widget _buildBody(RefreshController _refreshController,
+      Future<void> Function(VoidCallback? _refetch) _refreshList) {
+    return Query(
+      options: QueryOptions(
+        document: gql(commentsQuery),
+        variables: <String, dynamic>{
+          'article_id': articleId,
+        },
       ),
+      builder: (QueryResult result,
+          {VoidCallback? refetch, FetchMore? fetchMore}) {
+        if (result.hasException) {
+          return Text(result.exception.toString());
+        }
+
+        if (result.isLoading) {
+          return const LoadingIndicator();
+        }
+
+        var comments = <Comment>[];
+        final resultData = result.data;
+        if (resultData != null) {
+          comments = CommentListResponse.fromJson(resultData).comments;
+        }
+
+        return SmartRefresher(
+          controller: _refreshController,
+          onRefresh: () async {
+            await _refreshList(refetch);
+          },
+          child: _buildListView(comments),
+        );
+      },
     );
   }
 
