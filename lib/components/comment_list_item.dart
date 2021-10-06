f1d44743df57711ae057810287c3a8790413f342
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-10-06 13:17:05 +0900

    コメント一覧のレイアウトを修正

--- lib/components/comment_list_item.dart
+++ lib/components/comment_list_item.dart
@@ -15,58 +15,60 @@ class CommentListItem extends StatelessWidget {
     return GestureDetector(
       onTap: () {},
       child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 44,
-                    width: 44,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(44 / 2),
-                      child: CachedNetworkImage(
-                        fit: BoxFit.cover,
-                        imageUrl: comment.user?.profileImageUrl ?? '',
-                        errorWidget: (context, url, dynamic error) =>
-                            Image.asset('assets/images/default_article.png'),
-                      ),
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 44,
+              width: 44,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(44 / 2),
+                child: CachedNetworkImage(
+                  fit: BoxFit.cover,
+                  imageUrl: comment.user?.profileImageUrl ?? '',
+                  errorWidget: (context, url, dynamic error) =>
+                      Image.asset('assets/images/default_article.png'),
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      comment.user?.name ?? '',
+                      style: Theme.of(context).textTheme.subtitle2,
                     ),
+                    const SizedBox(width: 16),
+                    Text(
+                      comment.createdAtTimeAgo(),
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  comment.text,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                const SizedBox(width: 4),
+                GestureDetector(
+                  onTap: onTapMenuButton,
+                  child: Icon(
+                    Icons.more_horiz,
+                    color: AppColors().textSecondary,
+                    size: 24,
                   ),
-                ],
-              ),
-              const SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    comment.user?.name ?? '',
-                    style: TextStyle(color: AppColors().textPrimary),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    comment.text,
-                    style: TextStyle(color: AppColors().textPrimary),
-                  ),
-                  const SizedBox(height: 4),
-                  Row(
-                    children: [
-                      Text(
-                        comment.createdAtTimeAgo(),
-                        style: TextStyle(color: AppColors().textPrimary),
-                      ),
-                      const SizedBox(width: 8),
-                      IconButton(
-                          onPressed: onTapMenuButton,
-                          icon: const Icon(Icons.more_horiz)),
-                      // const Icon(Icons.more_horiz)
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          )),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
