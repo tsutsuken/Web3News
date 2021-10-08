88a2180784bf1367ea6a5208f4e41e0a8e084f40
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-10-08 11:51:20 +0900

    ログイン時にマイページが更新されない問題に対応

--- lib/pages/my_profile/my_profile_page_notifier.dart
+++ lib/pages/my_profile/my_profile_page_notifier.dart
@@ -4,27 +4,31 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:labo_flutter/models/app_user/app_user.dart';
 import 'package:labo_flutter/models/app_user/app_user_repository.dart';
 import 'package:labo_flutter/models/comment/comment.dart';
+import 'package:labo_flutter/providers/user_change_notifier_provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 final myProfilePageNotifierProvider = ChangeNotifierProvider.autoDispose(
   (ref) {
     final appUserRepository = ref.read(appUserRepositoryProvider);
-    return MyProfilePageNotifier(appUserRepository);
+    final currentUser = ref
+        .watch(userChangeNotifierProvider.select((value) => value.currentUser));
+    return MyProfilePageNotifier(appUserRepository, currentUser);
   },
 );
 
 class MyProfilePageNotifier extends ChangeNotifier {
-  MyProfilePageNotifier(this._appUserRepository) {
+  MyProfilePageNotifier(this._appUserRepository, this.currentUser) {
     fetchMyAppUser();
   }
   final AppUserRepository _appUserRepository;
+  final User? currentUser;
   final RefreshController refreshController = RefreshController();
 
   AsyncValue<List<Comment>> commentsValue = const AsyncValue.loading();
   AsyncValue<AppUser?> myAppUserValue = const AsyncValue.loading();
 
   Future<void> fetchMyAppUser() async {
-    final userId = FirebaseAuth.instance.currentUser?.uid;
+    final userId = currentUser?.uid;
     if (userId == null) {
       myAppUserValue = const AsyncValue.data(null);
       notifyListeners();
