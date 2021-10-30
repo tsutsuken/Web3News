cf16e25b2232c90482aaefd84261f1778e88ee4a
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-10-30 11:41:37 +0900

    test SignInPageのテストを追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:labo_flutter/main.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:labo_flutter/pages/sign_in/sign_in_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('test SignInPage', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: SignInPage(),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('ログイン'), findsOneWidget);
+    expect(find.text('新規登録'), findsNothing);
   });
 }
